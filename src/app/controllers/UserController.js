261fe20f0b76559a108d358f9d924d563257b8b8
Bootcamp Maykao <mayk@rocketseat.com.br>
2019-12-15 04:44:24 -0400

    12 Formulário de Atualização do Usuário

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -1,11 +1,23 @@
 const User = require('../models/User')
+const { formatCep, formatCpfCnpj } = require('../../lib/utils')
 
 module.exports = {
     registerForm(req, res) {
         return res.render("user/register")
     },
-    show(req, res) {
-        return res.send('ok, cadastrado!')
+    async show(req, res) {
+        const { userId: id } = req.session
+
+        const user = await User.findOne({ where: {id} })
+
+        if (!user) return res.render("user/register", {
+            error: "Usuário não encontrado!"
+        })
+
+        user.cpf_cnpj = formatCpfCnpj(user.cpf_cnpj)
+        user.cep = formatCep(user.cep)
+
+        return res.render('user/index', { user })
     },
     async post(req, res) {
 
