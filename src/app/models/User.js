edb2e0c3886649d5df9f5c882a1fe5eac5ab3c22
Bootcamp Maykao <mayk@rocketseat.com.br>
2019-12-15 05:23:04 -0400

    13 Atualizando Conta de Usuário

--- src/app/models/User.js
+++ src/app/models/User.js
@@ -51,5 +51,25 @@ module.exports = {
         }catch(err) {
             console.error(err)
         }
+    },
+    async update(id, fields) {
+        let query = "UPDATE users SET"
+
+        Object.keys(fields).map((key, index, array) => {
+            if((index + 1) < array.length) {
+                query = `${query}
+                    ${key} = '${fields[key]}',
+                `
+            } else {
+                // last iteration
+                query = `${query}
+                    ${key} = '${fields[key]}'
+                    WHERE id = ${id}
+                `
+            }
+        })
+
+        await db.query(query)
+        return
     }
 }
