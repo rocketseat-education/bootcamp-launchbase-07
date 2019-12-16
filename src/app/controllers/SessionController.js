a174ab38eb355bd0149347ce537db0ab1144e480
Bootcamp Maykao <mayk@rocketseat.com.br>
2019-12-16 01:03:28 -0400

    19 Validando a Recuperação de Senha

--- src/app/controllers/SessionController.js
+++ src/app/controllers/SessionController.js
@@ -63,5 +63,28 @@ module.exports = {
         }
 
 
+    },
+    resetForm(req, res) {
+        return res.render("session/password-reset", { token: req.query.token})
+    },
+    reset(req, res) {
+        const { email, password, passwordRepeat, token } = req.body
+        try {
+            
+
+            // cria um novo hash de senha
+
+            // atualiza o usuário
+
+            // avisa o usuário que ele tem uma nova senha
+
+
+        }catch(err) {
+            console.error(err)
+            return res.render("session/password-reset", {
+                error: "Erro inesperado, tente novamente!"
+            })
+        }
+
     }
 }
