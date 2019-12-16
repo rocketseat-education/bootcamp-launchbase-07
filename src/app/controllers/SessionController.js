c7009dd50d1afaa6abd709285c9a8ffa67ca29a7
Bootcamp Maykao <mayk@rocketseat.com.br>
2019-12-16 00:00:09 -0400

    17 Form de Recuperação de Senha

--- src/app/controllers/SessionController.js
+++ src/app/controllers/SessionController.js
@@ -10,5 +10,11 @@ module.exports = {
     logout(req,res) {
         req.session.destroy()
         return res.redirect("/")
+    },
+    forgotForm(req, res) {
+        return res.render("session/forgot-password")
+    },
+    forgot(req, res) {
+        
     }
 }
