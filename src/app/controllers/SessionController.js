8c1d76c65918c25aa92c663ff1bda200c39b373d
Bootcamp Maykao <mayk@rocketseat.com.br>
2019-12-15 21:47:36 -0400

    14 Menu do Usuário e Logout (e parte do 15)

---
+++ src/app/controllers/SessionController.js
@@ -0,0 +1,9 @@
+module.exports = {
+    loginForm(req, res) {
+        return res.render("session/index")
+    },
+    logout(req,res) {
+        req.session.destroy()
+        return res.redirect("/")
+    }
+}
--- src/routes/users.js
+++ src/routes/users.js
@@ -7,9 +7,9 @@ const UserController = require('../app/controllers/UserController')
 const Validator = require('../app/validators/user')
 
 // // login/logout 
-// routes.get('/login', SessionController.loginForm)
+routes.get('/login', SessionController.loginForm)
 // routes.post('/login', SessionController.login)
-// routes.post('/logout', SessionController.logout)
+routes.post('/logout', SessionController.logout)
 
 // // reset password / forgot
 // routes.get('/forgot-password', SessionController.forgotForm)
