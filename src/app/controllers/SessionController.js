024fc7fc179a5f75a37a375d303be8231cfba111
Bootcamp Maykao <mayk@rocketseat.com.br>
2019-12-16 01:18:31 -0400

    20 Criando Nova Senha

--- src/app/controllers/SessionController.js
+++ src/app/controllers/SessionController.js
@@ -1,5 +1,6 @@
 const User = require('../models/User')
 
+const { hash } = require('bcryptjs')
 const crypto = require('crypto')
 const mailer = require('../../lib/mailer')
 
@@ -67,21 +68,33 @@ module.exports = {
     resetForm(req, res) {
         return res.render("session/password-reset", { token: req.query.token})
     },
-    reset(req, res) {
-        const { email, password, passwordRepeat, token } = req.body
-        try {
-            
+    async reset(req, res) {
+        const user = req.user
 
+        const { password, token } = req.body
+        
+        try {
             // cria um novo hash de senha
+            const newPassword = await hash(password, 8)
 
             // atualiza o usuário
+            await User.update(user.id, {
+                password: newPassword,
+                reset_token: "",
+                reset_token_expires: "",
+            })
 
             // avisa o usuário que ele tem uma nova senha
-
+            return res.render("session/login", {
+                user:req.body,
+                success: "Senha atualizada! Faça o seu login"
+            })
 
         }catch(err) {
             console.error(err)
             return res.render("session/password-reset", {
+                user: req.body,
+                token,
                 error: "Erro inesperado, tente novamente!"
             })
         }
