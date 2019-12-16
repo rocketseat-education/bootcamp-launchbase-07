f08fbba9b060c828ef2d10859fe44737ba98c633
Bootcamp Maykao <mayk@rocketseat.com.br>
2019-12-16 00:31:52 -0400

    18 Lógica da Recuperação de Senha

--- database.sql
+++ database.sql
@@ -79,3 +79,7 @@ CREATE TABLE "session" (
 )
 WITH (OIDS=FALSE);
 ALTER TABLE "session" ADD CONSTRAINT "session_pkey" PRIMARY KEY ("sid") NOT DEFERRABLE INITIALLY IMMEDIATE;
+
+-- token password recovery
+ALTER TABLE "users" ADD COLUMN reset_token text;
+ALTER TABLE "users" ADD COLUMN reset_token_expires text;
