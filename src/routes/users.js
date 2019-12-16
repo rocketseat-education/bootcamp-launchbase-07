5319e12076e27532b63d3c58d01c159400a50238
Bootcamp Maykao <mayk@rocketseat.com.br>
2019-12-15 23:27:58 -0400

    16 Bloqueio de Rotas e Variáveis Globais no Express

--- src/routes/users.js
+++ src/routes/users.js
@@ -7,8 +7,10 @@ const UserController = require('../app/controllers/UserController')
 const UserValidator = require('../app/validators/user')
 const SessionValidator = require('../app/validators/session')
 
+const { isLoggedRedirectToUsers, onlyUsers } = require('../app/middlewares/session')
+
 // // login/logout 
-routes.get('/login', SessionController.loginForm)
+routes.get('/login', isLoggedRedirectToUsers, SessionController.loginForm)
 routes.post('/login', SessionValidator.login, SessionController.login)
 routes.post('/logout', SessionController.logout)
 
@@ -22,7 +24,7 @@ routes.post('/logout', SessionController.logout)
 routes.get('/register', UserController.registerForm)
 routes.post('/register', UserValidator.post, UserController.post)
 
-routes.get('/',UserValidator.show, UserController.show)
+routes.get('/', onlyUsers, UserValidator.show, UserController.show)
 routes.put('/', UserValidator.update, UserController.update)
 // routes.delete('/', UserController.delete)
 
